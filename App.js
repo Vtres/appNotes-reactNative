02134f621984853a0c2fe723d2179ac70a5473b8
Vtres <viniciust98@outlook.com>
2022-05-01 00:50:59 -0300

    configurando app.js

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import  Constants from 'expo-constants';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={{ backgroundColor: '#d8dfe6', marginTop: Constants.statusBarHeight }}> 
+      <View style={styles.container}>
+        <StatusBar backgroundColor="#d8dfe6" translucent />
+        <Text>Open up App.js to start working on your app!</Text>
+       
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
